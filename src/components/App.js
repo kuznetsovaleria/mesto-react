9dd9b152d20991db5a84193569f2e1d87731bc30
Valeria Kuznetsova <kuznetsovaleriaa@yandex.ru>
2021-02-01 22:43:44 +0300

    refactor: add Promise.all, replace submit btn into PopupWithForm component

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,64 @@
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
-import PopupWithForm from './PopupWithForm';
-import ImagePopup from './ImagePopup';
-import React from 'react'
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import PopupWithForm from "./PopupWithForm";
+import ImagePopup from "./ImagePopup";
+import React from "react";
 
 function App() {
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(
+    false
+  );
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(
+    false
+  );
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
+  const [isImagePopupOpen, setIsImagePopupOpen] = React.useState(false);
+  const [selectedCard, setSelectedCard] = React.useState({});
 
-    const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
-    const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
-    const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
-    const [isImagePopupOpen, setIsImagePopupOpen] = React.useState(false);
-    const [selectedCard, setSelectedCard] = React.useState({});
-
-    function handleEditAvatarClick() {
-      setIsEditAvatarPopupOpen(true)
-  };
-
-    function handleEditProfileClick() {
-      setIsEditProfilePopupOpen(true)
-  };
-
-    function handleAddPlaceClick() {
-      setIsAddPlacePopupOpen(true)
-  };
-
-    function handleCardClick(card) {
-      setSelectedCard(card);
-      setIsImagePopupOpen(true)
+  function handleEditAvatarClick() {
+    setIsEditAvatarPopupOpen(true);
   }
 
-    function closeAllPopups() {
-      setIsEditAvatarPopupOpen(false);
-      setIsEditProfilePopupOpen(false);
-      setIsAddPlacePopupOpen(false);
-      setSelectedCard({});
-      setIsImagePopupOpen(false);
+  function handleEditProfileClick() {
+    setIsEditProfilePopupOpen(true);
+  }
+
+  function handleAddPlaceClick() {
+    setIsAddPlacePopupOpen(true);
+  }
+
+  function handleCardClick(card) {
+    setSelectedCard(card);
+    setIsImagePopupOpen(true);
+  }
+
+  function closeAllPopups() {
+    setIsEditAvatarPopupOpen(false);
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setSelectedCard({});
+    setIsImagePopupOpen(false);
   }
 
   return (
     <div className="page">
       <Header />
-      <Main 
+      <Main
         onEditAvatar={handleEditAvatarClick}
         onEditProfile={handleEditProfileClick}
         onAddPlace={handleAddPlaceClick}
         onCardClick={handleCardClick}
       />
       <Footer />
-      <PopupWithForm title='Редактировать профиль' name='edit' isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
-        <input 
+      <PopupWithForm
+        title="Редактировать профиль"
+        name="edit"
+        isOpen={isEditProfilePopupOpen}
+        onClose={closeAllPopups}
+        onSubmit={`Сохранить`}
+      >
+        <input
           id="name-input"
           className="popup__input popup__input_name"
           type="text"
@@ -57,8 +66,9 @@ function App() {
           minLength="2"
           maxLength="40"
           placeholder="Имя"
-          required />
-          <span id="name-input-error" className="error" />
+          required
+        />
+        <span id="name-input-error" className="error" />
         <input
           id="profession-input"
           type="text"
@@ -67,12 +77,18 @@ function App() {
           minLength="2"
           maxLength="200"
           placeholder="Занятие"
-          required />
-          <span id="profession-input-error" className="error" />
-        <button type="submit" className="popup__submit">Сохранить</button>
+          required
+        />
+        <span id="profession-input-error" className="error" />
       </PopupWithForm>
 
-      <PopupWithForm title='Новое место' name='add' isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+      <PopupWithForm
+        title="Новое место"
+        name="add"
+        isOpen={isAddPlacePopupOpen}
+        onClose={closeAllPopups}
+        onSubmit={`Создать`}
+      >
         <input
           id="place-input"
           minLength="2"
@@ -81,38 +97,52 @@ function App() {
           name="place-name"
           placeholder="Название"
           className="popup__input popup__input_place"
-          required />
-          <span id="place-input-error" className="error" />
+          required
+        />
+        <span id="place-input-error" className="error" />
         <input
           id="link-input"
           type="url"
           name="img-link"
           placeholder="Ссылка на картинку"
           className="popup__input popup__input_link"
-          required />
-          <span id="link-input-error" className="error" />
-        <button type="submit" className="popup__submit popup__submit_add">Создать</button>
+          required
+        />
+        <span id="link-input-error" className="error" />
       </PopupWithForm>
 
-      <PopupWithForm title='Обновить аватар' name='change-avatar' isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
-        <input 
+      <PopupWithForm
+        title="Обновить аватар"
+        name="change-avatar"
+        isOpen={isEditAvatarPopupOpen}
+        onClose={closeAllPopups}
+        onSubmit={`Сохранить`}
+      >
+        <input
           id="link-avatar"
           type="url"
           name="avatar-link"
           placeholder="Ссылка на аватар"
           className="popup__input popup__input_link"
-          required />
-          <span id="link-avatar-error" className="error"/>
-        <button type="submit" className="popup__submit popup__submit_avatar">Сохранить</button>
+          required
+        />
+        <span id="link-avatar-error" className="error" />
       </PopupWithForm>
 
-      <PopupWithForm title='Вы уверены?' name='delete-place' isOpen={false} onClose={closeAllPopups}>
-        <button type="submit" className="popup__submit popup__submit_delete">Да</button>
-      </PopupWithForm>
+      <PopupWithForm
+        title="Вы уверены?"
+        name="delete-place"
+        isOpen={false}
+        onClose={closeAllPopups}
+        onSubmit={`Да`}
+      ></PopupWithForm>
 
-      <ImagePopup card={selectedCard} isOpen={isImagePopupOpen} onClose={closeAllPopups} />
-
-   </div>
+      <ImagePopup
+        card={selectedCard}
+        isOpen={isImagePopupOpen}
+        onClose={closeAllPopups}
+      />
+    </div>
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,26 @@
 function Card(props) {
+  function handleClick() {
+    props.onCardClick(props.card);
+  }
 
-    function handleClick() {
-        props.onCardClick(props.card)
-    }
-
-    return (
-        <article className="card" key={props.card_id}>
-        <img src={props.card.link} alt={props.card.name} className="card__photo" onClick={handleClick}/>
-        <button className="card__delete" type="button"></button>
-        <div className="card__group">
-          <h2 className="card__place">{props.card.name}</h2>
-          <div className ="card__likes">
-            <button className="card__like" type="button"></button>
-            <p className="card__likes-counter">{props.card.likes.length}</p>
-         </div>
+  return (
+    <article className="card" key={props.card_id}>
+      <img
+        src={props.card.link}
+        alt={props.card.name}
+        className="card__photo"
+        onClick={handleClick}
+      />
+      <button className="card__delete" type="button"></button>
+      <div className="card__group">
+        <h2 className="card__place">{props.card.name}</h2>
+        <div className="card__likes">
+          <button className="card__like" type="button"></button>
+          <p className="card__likes-counter">{props.card.likes.length}</p>
         </div>
-        </article>
-    )
+      </div>
+    </article>
+  );
 }
 
 export default Card;
